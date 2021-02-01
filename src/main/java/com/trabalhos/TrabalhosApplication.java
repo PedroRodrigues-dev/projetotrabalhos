4a012c4ae3295c54ecd15246d82e644f425de2bc
Pedro Rodrigues <pedroras004@gmail.com>
2021-02-01 15:18:42 -0300

    BASE

---
+++ src/main/java/com/trabalhos/TrabalhosApplication.java
@@ -0,0 +1,13 @@
+package com.trabalhos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhosApplication.class, args);
+	}
+
+}
